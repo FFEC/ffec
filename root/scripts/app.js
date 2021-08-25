ace20a6ece672b14fedeb214a12cf83f79ff7cc6
Kailey Waal <15kwaal@gmail.com>
2021-08-25 13:08:42 -0600

    Added functionality to update donation link with radio buttons

--- root/scripts/app.js
+++ root/scripts/app.js
@@ -1,4 +1,5 @@
 window.onload = toggleOtherDonationAmount();
+window.onload = updateDonationLink();
 
 function toggleMenu(x) {
     x.classList.toggle('collapsible--open');
@@ -22,3 +23,33 @@ function toggleOtherDonationAmount() {
 }
 
 document.querySelectorAll("div.donation-amount").forEach(button => button.addEventListener("click", toggleOtherDonationAmount));
+
+function updateDonationLink() {
+    let donationAmountRadioButtons = document.querySelectorAll("input[name=donation_amount]");
+    let checkedDonationAmountButton;
+    let donationAmount;
+    donationAmountRadioButtons.forEach(button => {
+        if(button.checked) {
+            checkedDonationAmountButton = button;
+        }
+    });
+    if(checkedDonationAmountButton.value != "other-amount") {
+        donationAmount = checkedDonationAmountButton.value;
+    } else {
+        let inputBox = document.querySelector("input[name=other_donation_amount]");
+        // trim off any white space
+        const inputValue = inputBox.value.trim();
+        console.log(inputValue);
+        // if inputValue is not empty
+        if(inputValue) {
+            donationAmount = inputValue;
+        } else {
+            donationAmount = 0;
+        }
+    }
+    let link = "https://www.paypal.com/donate?business=ffec17%40gmail.com&amount=" + donationAmount +"&no_recurring=0&item_name=Building+strong+foundations+for+self-sufficient+families.&currency_code=USD"
+    document.querySelector(".donate-button-link").href = link;
+}
+
+document.querySelectorAll("input[name=donation_amount]").forEach(button => button.addEventListener("click", updateDonationLink));
+document.querySelector("#other_donation_amount").addEventListener("keyup", updateDonationLink);
